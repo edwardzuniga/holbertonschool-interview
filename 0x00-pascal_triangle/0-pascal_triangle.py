b0abdf3f2ee6dd62c1fd10bf79d9b9052c8b4a70
edwardzuniga <4593@holbertonschool.com>
2022-11-30 18:21:41 -0500

    0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle function
+"""
+
+
+def pascal_triangle(n):
+    """ 
+    list of lists of integers representing
+    """
+    triangle_pascal = []
+    for i in range(1, n+1):
+        row = []
+        for j in range(i):
+            if j == 0 or j == i-1:
+                n = 1
+                row.append(n)
+            else:
+                n = triangle_pascal[i-2][j-1] + triangle_pascal[i-2][j]
+                row.append(n)
+        triangle_pascal.append(row)
+
+    return triangle_pascal
